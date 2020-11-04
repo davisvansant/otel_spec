6137c99a4dad43b49bd35fe149c2cd4c1babaf67
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-04 13:07:48 -0800

    moving span closer to spec impl

--- api/src/tracing/span.rs
+++ api/src/tracing/span.rs
@@ -1,5 +1,12 @@
+use crate::tracing::span::events::Event;
+use crate::tracing::span::status::{Status, StatusCode};
 use crate::tracing::span_context::SpanContext;
+use std::time::{Duration, SystemTime};
 
+mod events;
+pub mod status;
+
+#[derive(Debug, PartialEq, PartialOrd)]
 pub enum ParentSpan {
     Span,
     SpanContext,
@@ -10,35 +17,148 @@ pub struct Span {
     pub span_context: SpanContext,
     pub parent_span: ParentSpan,
     pub span_kind: String,
-    pub start_timestamp: u16,
-    pub stop_timestamp: u16,
+    pub start_timestamp: SystemTime,
+    pub stop_timestamp: Option<SystemTime>,
     pub attributes: Vec<String>,
     pub link: Vec<String>,
-    pub events: Vec<String>,
-    pub status: String,
+    events: Vec<Event>,
+    status: Status,
 }
 
 impl Span {
-    pub fn create(
+    pub(crate) fn create(
         name: String,
-        span_context: SpanContext,
+        // span_context: SpanContext,
         parent_span: ParentSpan,
         span_kind: String,
-        start_timestamp: u16,
-        stop_timestamp: u16,
-        status: String,
+        // start_timestamp: u16,
+        // stop_timestamp: u16,
+        // status: Status,
     ) -> Span {
         Span {
             name,
-            span_context,
+            span_context: SpanContext::create(),
             parent_span,
             span_kind,
-            start_timestamp,
-            stop_timestamp,
+            start_timestamp: SystemTime::now(),
+            stop_timestamp: None,
             attributes: Vec::new(),
             link: Vec::new(),
             events: Vec::new(),
-            status,
+            status: Status::default(),
         }
+    }
+
+    pub fn get_context(&self) -> &SpanContext {
+        &self.span_context
+    }
+
+    pub fn is_recording(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn set_attributes(&mut self) {
+        unimplemented!()
+    }
+
+    pub fn add_event(&mut self, name: String) {
+        let event = Event::new(name);
+        self.events.push(event)
+    }
+
+    pub fn set_status(&mut self, status: StatusCode, description: Option<String>) {
+        self.status.set(status, description);
+    }
+
+    pub fn update_name(&mut self, name: String) {
+        self.name = name
+    }
+
+    pub fn end(&mut self) {
+        self.stop_timestamp = Some(SystemTime::now())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let span = Span::create(
+            String::from("test_span"),
+            ParentSpan::Span,
+            String::from("test_span_kind"),
+        );
+
+        assert_eq!(span.name, String::from("test_span"));
+        assert_eq!(span.span_context.trace_id.is_nil(), false);
+        assert_eq!(span.span_context.span_id.is_nil(), false);
+        assert_eq!(span.span_context.trace_flags.is_empty(), true);
+        assert!(span.span_context.trace_state.trace_state.is_empty());
+        assert_eq!(span.parent_span, ParentSpan::Span);
+        assert_eq!(span.span_kind, String::from("test_span_kind"));
+        assert_ne!(span.start_timestamp, SystemTime::now());
+        assert!(span.stop_timestamp.is_none());
+        assert!(span.attributes.is_empty());
+        assert!(span.link.is_empty());
+        assert!(span.events.is_empty());
+        assert_eq!(span.status.status_code, StatusCode::Unset);
+        assert!(span.status.description.is_none());
+    }
+
+    #[test]
+    fn get_context() {
+        let span = Span::create(
+            String::from("test_span"),
+            ParentSpan::Span,
+            String::from("test_span_kind"),
+        );
+
+        let context = span.get_context();
+        assert_eq!(context.trace_id.is_nil(), false);
+        assert_eq!(context.span_id.is_nil(), false);
+        assert_eq!(context.trace_flags.is_empty(), true);
+        assert!(context.trace_state.trace_state.is_empty());
+    }
+
+    // #[test]
+    // fn is_recording() {
+    //     unimplemented!()
+    // }
+
+    // #[test]
+    // fn set_attributes() {
+    //     unimplemented!()
+    // }
+
+    #[test]
+    fn add_event() {
+        let mut span = Span::create(
+            String::from("test_span"),
+            ParentSpan::Span,
+            String::from("test_span_kind"),
+        );
+
+        assert_eq!(span.events.len(), 0);
+        span.add_event(String::from("test_event"));
+        assert_eq!(span.events.len(), 1);
+    }
+
+    #[test]
+    fn set_status() {
+        let mut span = Span::create(
+            String::from("test_span"),
+            ParentSpan::Span,
+            String::from("test_span_kind"),
+        );
+
+        assert_eq!(span.status.status_code, StatusCode::Unset);
+        span.set_status(StatusCode::Ok, None);
+        assert_ne!(span.status.status_code, StatusCode::Unset);
+        assert_eq!(span.status.status_code, StatusCode::Ok);
+        span.set_status(StatusCode::Error, None);
+        assert_ne!(span.status.status_code, StatusCode::Ok);
+        assert_eq!(span.status.status_code, StatusCode::Error);
     }
 }
