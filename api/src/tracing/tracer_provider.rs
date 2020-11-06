b4b982c89c134d30c3887f97861ef45e7ee113a8
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-05 20:57:02 -0800

    initial tracer_provider impl

--- api/src/tracing/tracer_provider.rs
+++ api/src/tracing/tracer_provider.rs
@@ -1,9 +1,48 @@
+use crate::tracing::span::Span;
+use crate::tracing::tracer::Tracer;
+
 pub struct TracerProvider {
-    pub tracer: Vec<String>,
+    name: &'static str,
+    version: &'static str,
+    tracer: Tracer,
 }
 
 impl TracerProvider {
-    pub fn get_tracer() {
-        unimplemented!()
+    pub fn default() -> TracerProvider {
+        TracerProvider {
+            name: env!("CARGO_PKG_NAME"),
+            version: env!("CARGO_PKG_VERSION"),
+            tracer: Tracer::init(),
+        }
+    }
+
+    fn get_tracer(&mut self) -> &mut Tracer {
+        &mut self.tracer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default() {
+        let global = TracerProvider::default();
+        assert_eq!(global.name, env!("CARGO_PKG_NAME"));
+        assert_eq!(global.version, env!("CARGO_PKG_VERSION"));
+        assert!(global.tracer.trace.is_empty());
+        assert_eq!(global.tracer.trace.len(), 0);
+    }
+
+    #[test]
+    fn get_tracer() {
+        let mut global = TracerProvider::default();
+        let mut tracer = global.get_tracer();
+        assert!(tracer.trace.is_empty());
+        assert_eq!(tracer.trace.len(), 0);
+
+        tracer.create_tracer();
+        assert_eq!(tracer.trace.is_empty(), false);
+        assert_eq!(tracer.trace.len(), 1);
     }
 }
