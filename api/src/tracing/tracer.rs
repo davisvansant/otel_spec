8ecea2ad9e66d7b4be73d8fe4ebb0c9139438302
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-05 20:55:36 -0800

    adjust tracer to currently not take any args

--- api/src/tracing/tracer.rs
+++ api/src/tracing/tracer.rs
@@ -3,37 +3,39 @@ use crate::tracing::span::span_kind::SpanKind;
 use crate::tracing::span::status::Status;
 use crate::tracing::span::ParentSpan;
 use crate::tracing::span::Span;
+use crate::SystemTime;
 
 pub struct Tracer {
-    pub span: Vec<Span>,
+    pub trace: Vec<Span>,
 }
 
 impl Tracer {
     pub fn init() -> Tracer {
         // unimplemented!()
-        Tracer { span: Vec::new() }
+        Tracer { trace: Vec::new() }
     }
 
     pub fn create_tracer(
         &mut self,
-        name: String,
+        // name: String,
         // span_context: SpanContext,
-        parent_span: ParentSpan,
-        span_kind: Option<SpanKind>,
+        // parent_span: ParentSpan,
+        // span_kind: Option<SpanKind>,
         // start_timestamp: u16,
         // stop_timestamp: u16,
         // status: Status,
     ) {
-        let span = Span::create(
-            name,
-            // span_context,
-            parent_span,
-            span_kind,
-            // start_timestamp,
-            // stop_timestamp,
-            // status,
-        );
-        self.span.push(span)
+        // let span = Span::create(
+        //     // name,
+        //     // span_context,
+        //     parent_span,
+        //     // span_kind,
+        //     // start_timestamp,
+        //     // stop_timestamp,
+        //     // status,
+        // );
+        let span = Span::create();
+        self.trace.push(span)
     }
 }
 
@@ -44,46 +46,48 @@ mod tests {
     #[test]
     fn init() {
         let tracer = Tracer::init();
-        assert!(tracer.span.is_empty());
-        assert_eq!(tracer.span.len(), 0);
+        assert!(tracer.trace.is_empty());
+        assert_eq!(tracer.trace.len(), 0);
     }
 
     #[test]
-    fn create() {
+    fn create_span() {
+        // let mut tracer: Span = Tracer::create_span(ParentSpan::Span);
+        // let mut tracer: Span = Tracer::create_span();
         let mut tracer = Tracer::init();
         tracer.create_tracer(
-            String::from("test_span_one"),
-            ParentSpan::Span,
-            // String::from("test_span_kind"),
-            None,
-        );
-        assert_eq!(tracer.span.is_empty(), false);
-        assert_eq!(tracer.span.len(), 1);
-
-        tracer.create_tracer(
-            String::from("test_span_two"),
-            ParentSpan::Span,
-            // String::from("test_span_kind"),
-            None,
-        );
-        assert_eq!(tracer.span.len(), 2);
-
-        tracer.create_tracer(
-            String::from("test_span_three"),
-            ParentSpan::Span,
+            // String::from("test_span_one"),
+            // ParentSpan::Span,
             // String::from("test_span_kind"),
             // None,
-            Some(SpanKind::Producer),
         );
-        assert_eq!(tracer.span.len(), 3);
+        assert_eq!(tracer.trace.is_empty(), false);
+        assert_eq!(tracer.trace.len(), 1);
 
         tracer.create_tracer(
-            String::from("test_span_three"),
-            ParentSpan::Span,
+            // String::from("test_span_two"),
+            // ParentSpan::Span,
             // String::from("test_span_kind"),
             // None,
-            Some(SpanKind::Consumer),
         );
-        assert_eq!(tracer.span.len(), 4);
+        assert_eq!(tracer.trace.len(), 2);
+
+        tracer.create_tracer(
+            // String::from("test_span_three"),
+            // ParentSpan::Span,
+            // String::from("test_span_kind"),
+            // None,
+            // Some(SpanKind::Producer),
+        );
+        assert_eq!(tracer.trace.len(), 3);
+
+        tracer.create_tracer(
+            // String::from("test_span_three"),
+            // ParentSpan::Span,
+            // String::from("test_span_kind"),
+            // None,
+            // Some(SpanKind::Consumer),
+        );
+        assert_eq!(tracer.trace.len(), 4);
     }
 }
