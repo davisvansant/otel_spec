647bad3224c9208436d6c40a6d928f16ebb26e04
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-08 15:10:48 -0800

    initial tests for trace_state

--- api/src/tracing/span/span_context/trace_state.rs
+++ api/src/tracing/span/span_context/trace_state.rs
@@ -41,4 +41,53 @@ mod tests {
         let trace_state = TraceState::create();
         assert!(trace_state.trace_state.is_empty());
     }
+
+    #[test]
+    fn get() {
+        let mut trace_state = TraceState::create();
+        trace_state
+            .trace_state
+            .insert(String::from("test_key"), String::from("test_value"));
+        let test_get = trace_state.get(String::from("test_key"));
+        assert_eq!(test_get.unwrap(), &String::from("test_value"));
+    }
+
+    #[test]
+    fn add() {
+        let mut trace_state = TraceState::create();
+        assert_eq!(trace_state.trace_state.len(), 0);
+        let test_key = String::from("test_key");
+        let test_value = String::from("test_value");
+        trace_state.add(test_key, test_value);
+        assert_eq!(trace_state.trace_state.len(), 1);
+    }
+
+    #[test]
+    fn update() {
+        let mut trace_state = TraceState::create();
+        let test_key = String::from("test_key");
+        let test_value = String::from("test_value");
+        trace_state.add(test_key, test_value);
+        let tested_value = trace_state.get(String::from("test_key")).unwrap();
+        assert_eq!(tested_value, &String::from("test_value"));
+        let updated_key = String::from("test_key");
+        let updated_value = String::from("test_updated_value");
+        trace_state.update(updated_key, updated_value);
+        let test_updated_value = trace_state.get(String::from("test_key")).unwrap();
+        assert_eq!(test_updated_value, &String::from("test_updated_value"));
+    }
+
+    #[test]
+    fn delete() {
+        let mut trace_state = TraceState::create();
+        let test_key = String::from("test_key");
+        let test_value = String::from("test_value");
+        trace_state.add(test_key, test_value);
+        let test_get = trace_state.get(String::from("test_key"));
+        assert_eq!(test_get.unwrap(), &String::from("test_value"));
+        let delete_key = String::from("test_key");
+        trace_state.delete(&delete_key);
+        let test_get_again = trace_state.get(String::from("test_key"));
+        assert_eq!(test_get_again, None);
+    }
 }
