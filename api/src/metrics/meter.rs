6c62141bbf334060461c9127f396147c96b10760
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-11 11:18:39 -0800

    add name to meter, updated tests

--- api/src/metrics/meter.rs
+++ api/src/metrics/meter.rs
@@ -1,13 +1,15 @@
 use crate::metrics::instruments::Instrument;
 
 pub struct Meter {
+    pub name: String,
     pub metric_instrument: Instrument,
 }
 
 impl Meter {
-    pub fn new(instrument: Instrument) -> Meter {
+    pub fn new(name: String, metric_instrument: Instrument) -> Meter {
         Meter {
-            metric_instrument: instrument,
+            name,
+            metric_instrument,
         }
     }
 }
@@ -18,20 +20,32 @@ mod tests {
 
     #[test]
     fn new() {
-        let counter = Meter::new(Instrument::Counter);
+        let counter = Meter::new(String::from("test_counter"), Instrument::Counter);
         assert_eq!(counter.metric_instrument, Instrument::Counter);
-        let up_down_counter = Meter::new(Instrument::UpDownCounter);
+        let up_down_counter = Meter::new(
+            String::from("test_up_down_counter"),
+            Instrument::UpDownCounter,
+        );
         assert_eq!(up_down_counter.metric_instrument, Instrument::UpDownCounter);
-        let value_recorder = Meter::new(Instrument::ValueRecorder);
+        let value_recorder = Meter::new(
+            String::from("test_value_recorder"),
+            Instrument::ValueRecorder,
+        );
         assert_eq!(value_recorder.metric_instrument, Instrument::ValueRecorder);
-        let sum_observer = Meter::new(Instrument::SumObserver);
+        let sum_observer = Meter::new(String::from("test_sum_observer"), Instrument::SumObserver);
         assert_eq!(sum_observer.metric_instrument, Instrument::SumObserver);
-        let up_down_sum_observer = Meter::new(Instrument::UpDownSumObserver);
+        let up_down_sum_observer = Meter::new(
+            String::from("test_up_down_counter"),
+            Instrument::UpDownSumObserver,
+        );
         assert_eq!(
             up_down_sum_observer.metric_instrument,
-            Instrument::UpDownSumObserver
+            Instrument::UpDownSumObserver,
         );
-        let value_observer = Meter::new(Instrument::ValueObserver);
+        let value_observer = Meter::new(
+            String::from("test_value_observer"),
+            Instrument::ValueObserver,
+        );
         assert_eq!(value_observer.metric_instrument, Instrument::ValueObserver);
     }
 }
