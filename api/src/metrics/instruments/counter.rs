7c5e66c3439f525e150450d975b3e75c322fc0d6
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-12 11:15:17 -0800

    some counter tests, trying not to cmp f64s

--- api/src/metrics/instruments/counter.rs
+++ api/src/metrics/instruments/counter.rs
@@ -28,10 +28,16 @@ mod tests {
     fn new() {
         let mut counter = Counter::default();
         assert!(counter.measurement.is_empty());
+        assert_eq!(counter.measurement.iter().sum::<f64>().to_bits(), 0);
 
         for f in 0..50 {
             counter.add(f as f64)
         }
         assert_eq!(counter.measurement.len(), 50);
+        assert!(counter.measurement.iter().sum::<f64>().is_normal());
+        assert_eq!(
+            counter.measurement.iter().sum::<f64>().to_bits(),
+            4653102422422454272
+        );
     }
 }
