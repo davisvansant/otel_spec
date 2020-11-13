1e79737f58dee348bf7a6195f0219ebbe6dd6911
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-13 12:18:58 -0800

    initial value recorder

--- api/src/metrics/instruments.rs
+++ api/src/metrics/instruments.rs
@@ -1,5 +1,6 @@
 pub mod counter;
 pub mod up_down_counter;
+pub mod value_recorder;
 
 #[derive(Debug, PartialEq)]
 pub enum Instrument {
---
+++ api/src/metrics/instruments/value_recorder.rs
@@ -0,0 +1,44 @@
+pub struct ValueRecorder {
+    pub measurement: Vec<u64>,
+}
+
+impl ValueRecorder {
+    pub fn default() -> ValueRecorder {
+        ValueRecorder {
+            measurement: Vec::with_capacity(50),
+        }
+    }
+
+    pub fn record(&mut self, value: u64) {
+        self.measurement.push(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default() {
+        let value_recorder = ValueRecorder::default();
+        assert!(value_recorder.measurement.is_empty());
+    }
+
+    #[test]
+    fn record() {
+        let mut value_recorder = ValueRecorder::default();
+        assert!(value_recorder.measurement.is_empty());
+
+        for n in 1..=50 {
+            value_recorder.record(n);
+        }
+
+        let min: u64 = 1;
+        let max: u64 = 50;
+
+        assert_eq!(value_recorder.measurement.len(), 50);
+        assert_eq!(value_recorder.measurement.iter().count(), 50);
+        assert_eq!(value_recorder.measurement.iter().min().unwrap(), &min);
+        assert_eq!(value_recorder.measurement.iter().max().unwrap(), &max);
+    }
+}
