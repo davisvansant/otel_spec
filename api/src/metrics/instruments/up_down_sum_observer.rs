c07488c369bd5bf01ef4a95c407ef891915276d2
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-15 14:15:43 -0800

    initial up down sum observer

---
+++ api/src/metrics/instruments/up_down_sum_observer.rs
@@ -0,0 +1,68 @@
+use super::Increment;
+
+pub struct UpDownSumObserver {
+    pub measurement: Vec<i64>,
+}
+
+impl UpDownSumObserver {
+    pub fn default() -> UpDownSumObserver {
+        UpDownSumObserver {
+            measurement: Vec::with_capacity(50),
+        }
+    }
+
+    pub async fn observe(&mut self, sum: Increment) {
+        if sum == Increment::Up {
+            self.measurement.push(1 as i64)
+        } else {
+            self.measurement.push(-1 as i64)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default() {
+        let up_down_sum_obvserer = UpDownSumObserver::default();
+        assert!(up_down_sum_obvserer.measurement.is_empty());
+    }
+
+    #[tokio::test]
+    async fn observe_positive() {
+        let mut up_down_sum_obvserer = UpDownSumObserver::default();
+        assert!(up_down_sum_obvserer.measurement.is_empty());
+
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+
+        assert_eq!(up_down_sum_obvserer.measurement.len(), 8);
+        assert_eq!(up_down_sum_obvserer.measurement.iter().sum::<i64>(), 2);
+    }
+
+    #[tokio::test]
+    async fn observe_negative() {
+        let mut up_down_sum_obvserer = UpDownSumObserver::default();
+        assert!(up_down_sum_obvserer.measurement.is_empty());
+
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Up).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+        up_down_sum_obvserer.observe(Increment::Down).await;
+
+        assert_eq!(up_down_sum_obvserer.measurement.len(), 8);
+        assert_eq!(up_down_sum_obvserer.measurement.iter().sum::<i64>(), -2);
+    }
+}
