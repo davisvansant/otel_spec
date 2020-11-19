437553901152ec6cf6ec40832e885df68b5fdc73
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-19 12:44:45 -0800

    move span exporter to move generic

--- sdk/src/trace/span_exporter.rs
+++ sdk/src/trace/span_exporter.rs
@@ -1,37 +1,47 @@
 use api::tracing::span::Span;
 use api::tracing::tracer_provider::TracerProvider;
 
+pub enum ExportResult {
+    Success,
+    Failure,
+}
+
 pub trait SpanExporter {
-    fn export(&self) -> Vec<Span>;
+    // fn export(&self) -> Vec<Span>;
+    fn export(batch: TracerProvider) -> ExportResult;
     fn shutdown();
 }
 
-impl SpanExporter for TracerProvider {
-    fn export(&self) -> Vec<Span> {
-        self.tracer.trace.to_vec()
-    }
+// impl SpanExporter for TracerProvider {
+//     // fn export(&self) -> Vec<Span> {
+//     //     self.tracer.trace.to_vec()
+//     // }
+//     fn export(batch: TracerProvider) -> Vec<Span> {
+//         // self.tracer.trace.to_vec()
+//         batch.tracer.trace.to_vec()
+//     }
+//
+//     fn shutdown() {
+//         unimplemented!()
+//     }
+// }
 
-    fn shutdown() {
-        unimplemented!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::trace::tracer_provider::TracerCreation;
-
-    #[test]
-    fn export() {
-        let mut global = TracerProvider::default();
-        let test_trace_one_name = "test_trace_one";
-        let test_trace_one_version = "1.0.0";
-        global.create_tracer(test_trace_one_name, test_trace_one_version);
-        let test_trace_two_name = "test_trace_two";
-        let test_trace_two_version = "1.0.0";
-        global.create_tracer(test_trace_two_name, test_trace_two_version);
-        assert_eq!(global.tracer.trace.len(), 2);
-        let mut export = global.export();
-        assert_eq!(export.len(), 2);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::trace::tracer_provider::TracerCreation;
+//
+//     #[test]
+//     fn export() {
+//         let mut global = TracerProvider::default();
+//         let test_trace_one_name = "test_trace_one";
+//         let test_trace_one_version = "1.0.0";
+//         global.create_tracer(test_trace_one_name, test_trace_one_version);
+//         let test_trace_two_name = "test_trace_two";
+//         let test_trace_two_version = "1.0.0";
+//         global.create_tracer(test_trace_two_name, test_trace_two_version);
+//         assert_eq!(global.tracer.trace.len(), 2);
+//         let mut export = global.export();
+//         assert_eq!(export.len(), 2);
+//     }
+// }
