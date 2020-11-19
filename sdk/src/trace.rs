9e5ded315d61fc970ed926ff5b7f92a36ed26dbc
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-18 16:28:00 -0800

    initial export for span_exporter

--- sdk/src/trace.rs
+++ sdk/src/trace.rs
@@ -1,3 +1,4 @@
 pub mod sampler;
+pub mod span_exporter;
 pub mod span_processor;
 pub mod tracer_provider;
---
+++ sdk/src/trace/span_exporter.rs
@@ -0,0 +1,37 @@
+use api::tracing::span::Span;
+use api::tracing::tracer_provider::TracerProvider;
+
+pub trait SpanExporter {
+    fn export(&self) -> Vec<Span>;
+    fn shutdown();
+}
+
+impl SpanExporter for TracerProvider {
+    fn export(&self) -> Vec<Span> {
+        self.tracer.trace.to_vec()
+    }
+
+    fn shutdown() {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::trace::tracer_provider::TracerCreation;
+
+    #[test]
+    fn export() {
+        let mut global = TracerProvider::default();
+        let test_trace_one_name = "test_trace_one";
+        let test_trace_one_version = "1.0.0";
+        global.create_tracer(test_trace_one_name, test_trace_one_version);
+        let test_trace_two_name = "test_trace_two";
+        let test_trace_two_version = "1.0.0";
+        global.create_tracer(test_trace_two_name, test_trace_two_version);
+        assert_eq!(global.tracer.trace.len(), 2);
+        let mut export = global.export();
+        assert_eq!(export.len(), 2);
+    }
+}
