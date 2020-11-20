ea859d19103043680ac57cb71d3be669e877612f
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-20 11:59:40 -0800

    still working on span_processor, trying a struct/vec approach

--- sdk/src/trace/span_processor.rs
+++ sdk/src/trace/span_processor.rs
@@ -1,28 +1,62 @@
 // use api::tracing::tracer::Tracer;
 // use api::tracing::span::StatusCode;
+use api::tracing::span::Span;
 use api::tracing::tracer_provider::TracerProvider;
 
-trait SpanProcessor {
-    fn on_start();
-    fn on_end();
-    fn shutdown();
-    fn force_flush();
+pub struct SpanProcessor {
+    processor: Vec<Span>,
 }
 
-impl SpanProcessor for TracerProvider {
-    fn on_start() {
-        unimplemented!();
-    }
-    fn on_end() {
-        unimplemented!();
-    }
-    fn shutdown() {
-        unimplemented!();
-    }
-    fn force_flush() {
-        unimplemented!();
+impl SpanProcessor {
+    pub fn init() -> SpanProcessor {
+        SpanProcessor {
+            processor: Vec::with_capacity(10),
+        }
     }
 }
+// trait SpanProcessor {
+//     fn on_start();
+//     fn on_end();
+//     fn shutdown();
+//     fn force_flush();
+//     fn simple(exporter: String);
+//     fn batching(
+//         exporter: String,
+//         max_queue_size: u8,
+//         scheduled_delay_millis: u8,
+//         export_timeout_millis: u8,
+//         max_export_batch_size: u8,
+//     );
+// }
+
+// impl SpanProcessor for TracerProvider {
+//     fn on_start() {
+//         unimplemented!();
+//     }
+//     fn on_end() {
+//         unimplemented!();
+//     }
+//     fn shutdown() {
+//         unimplemented!();
+//     }
+//     fn force_flush() {
+//         unimplemented!();
+//     }
+//     fn simple(&mut self, exporter: String) {
+//         self.tracer.
+//     }
+// }
+
+// pub trait BuiltIn {
+//     fn simple(exporter: String);
+//     fn batching(
+//         exporter: String,
+//         max_queue_size: u8,
+//         scheduled_delay_millis: u8,
+//         export_timeout_millis: u8,
+//         max_export_batch_size: u8,
+//     );
+// }
 
 #[cfg(test)]
 mod tests {
