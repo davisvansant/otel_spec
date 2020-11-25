0b4ae299c3ff8ce396ecc37176b1e071c8371a8b
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-25 15:51:52 -0800

    initial accumulator

--- sdk/src/metrics/accumulator.rs
+++ sdk/src/metrics/accumulator.rs
@@ -1,1 +1,22 @@
-// unimplemented!()
+pub struct Accumulator {
+    pub aggregator: Vec<String>,
+}
+
+impl Accumulator {
+    pub fn init() -> Accumulator {
+        Accumulator {
+            aggregator: Vec::with_capacity(10),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        let test_accumulator = Accumulator::init();
+        assert_eq!(test_accumulator.aggregator.len(), 0);
+    }
+}
