5fe642071e464418a66988300a7f6006c909f1a2
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-24 17:42:06 -0800

    some initial export() functionality for span_exporter

--- sdk/src/trace/span_exporter.rs
+++ sdk/src/trace/span_exporter.rs
@@ -1,7 +1,7 @@
-use crate::trace::span_processor::BatchingProcessor;
+// use crate::trace::span_processor::BatchingProcessor;
 use crate::trace::span_processor::SpanProcessor;
 use api::tracing::span::Span;
-use api::tracing::tracer_provider::TracerProvider;
+// use api::tracing::tracer_provider::TracerProvider;
 
 #[derive(Debug, PartialEq)]
 pub enum Exporter {
@@ -18,13 +18,13 @@ pub enum ExportResult {
     Failure,
 }
 
-pub struct SpanExporter {
-    exporter: Exporter,
-    batch: Vec<Span>,
+pub struct SpanExporter<'a> {
+    pub exporter: Exporter,
+    pub batch: Vec<&'a Span>,
 }
 
-impl SpanExporter {
-    pub fn init(exporter: Exporter) -> SpanExporter {
+impl<'a> SpanExporter<'a> {
+    pub fn init(exporter: Exporter) -> SpanExporter<'a> {
         SpanExporter {
             exporter,
             batch: Vec::with_capacity(10),
@@ -32,12 +32,24 @@ impl SpanExporter {
     }
 }
 
-// pub trait SpanExporter {
-//     // fn export(&self) -> Vec<Span>;
-//     fn export(&mut self, batch: SpanProcessor) -> ExportResult;
-//     fn shutdown();
-// }
-//
+pub trait ExporterInterface<'a> {
+    fn export(&mut self, batch: &'a mut SpanProcessor) -> ExportResult;
+    fn shutdown();
+}
+
+impl<'a> ExporterInterface<'a> for SpanExporter<'a> {
+    fn export(&mut self, batch: &'a mut SpanProcessor) -> ExportResult {
+        let span = batch.collection.drain(..);
+        for s in span.as_slice().iter() {
+            self.batch.push(s.0);
+        }
+
+        ExportResult::Success
+    }
+
+    fn shutdown() {}
+}
+// //
 // impl SpanExporter for SpanProcessor {
 //     // fn export(&self) -> Vec<Span> {
 //     //     self.tracer.trace.to_vec()
@@ -56,11 +68,33 @@ impl SpanExporter {
 mod tests {
     use super::*;
     // use crate::trace::tracer_provider::TracerCreation;
+    use crate::trace::tracer_provider::TracerCreation;
+    use api::tracing::tracer_provider::TracerProvider;
 
     #[test]
     fn init() {
         let exporter = Exporter::InMemory;
         let test_span_exporter = SpanExporter::init(exporter);
         assert_eq!(test_span_exporter.batch.len(), 0);
+    }
+
+    #[test]
+    fn export() {
+        let mut global = TracerProvider::default();
+        let test_name = env!("CARGO_PKG_NAME");
+        let test_version = env!("CARGO_PKG_VERSION");
+        global.create_tracer(test_name, test_version);
+        let capacity: u16 = 2048;
+        let mut processor = SpanProcessor::init(capacity);
+        for span in global.tracer.trace.iter() {
+            let test_span = &span;
+            let test_parent_context = &span.span_context;
+            processor.on_start(test_span, test_parent_context);
+        }
+        let exporter = Exporter::InMemory;
+        let mut test_span_exporter = SpanExporter::init(exporter);
+        assert_eq!(test_span_exporter.batch.len(), 0);
+        test_span_exporter.export(&mut processor);
+        assert_eq!(test_span_exporter.batch.len(), 1);
     }
 }
