b63c1822b571503356dbc7222f258502a7381630
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-25 15:17:41 -0800

    add new_meter for meter provider

--- api/src/metrics/meter_provider.rs
+++ api/src/metrics/meter_provider.rs
@@ -1,9 +1,10 @@
-use crate::metrics::meter::Meter;
+use crate::metrics::meter::{Instrument, Meter};
 
 pub struct MeterProvider {
     pub name: &'static str,
     pub version: &'static str,
     pub meter: Vec<Meter>,
+    pub shutdown: bool,
 }
 
 impl MeterProvider {
@@ -12,7 +13,13 @@ impl MeterProvider {
             name: env!("CARGO_PKG_NAME"),
             version: env!("CARGO_PKG_VERSION"),
             meter: Vec::with_capacity(1),
+            shutdown: false,
         }
+    }
+
+    pub fn new_meter(&mut self, name: String, metric_instrument: Instrument) {
+        let m = Meter::new(name, metric_instrument);
+        self.meter.push(m);
     }
 }
 
@@ -22,7 +29,18 @@ mod tests {
 
     #[test]
     fn default() {
-        let global_meter = MeterProvider::default();
+        let test_meter = MeterProvider::default();
+        assert_eq!(test_meter.name, env!("CARGO_PKG_NAME"));
+        assert_eq!(test_meter.version, env!("CARGO_PKG_VERSION"));
+        assert_eq!(test_meter.meter.len(), 0);
+        assert_eq!(test_meter.shutdown, false);
+    }
+
+    #[test]
+    fn new_meter() {
+        let mut global_meter = MeterProvider::default();
         assert_eq!(global_meter.meter.len(), 0);
+        global_meter.new_meter(String::from("test_meter"), Instrument::Counter);
+        assert_eq!(global_meter.meter.len(), 1);
     }
 }
