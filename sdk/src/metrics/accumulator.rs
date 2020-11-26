edb88588842ce4db5db7cf0e67e7066cdda6afa1
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-26 12:01:40 -0800

    add generic accumluation structure for accumulator

--- sdk/src/metrics/accumulator.rs
+++ sdk/src/metrics/accumulator.rs
@@ -1,11 +1,13 @@
 pub struct Accumulator {
     pub aggregator: Vec<String>,
+    pub accumulation: (String, String),
 }
 
 impl Accumulator {
     pub fn init() -> Accumulator {
         Accumulator {
             aggregator: Vec::with_capacity(10),
+            accumulation: (String::with_capacity(32), String::with_capacity(32)),
         }
     }
 }
@@ -18,5 +20,9 @@ mod tests {
     fn init() {
         let test_accumulator = Accumulator::init();
         assert_eq!(test_accumulator.aggregator.len(), 0);
+        assert_eq!(
+            test_accumulator.accumulation,
+            (String::from(""), String::from(""))
+        );
     }
 }
