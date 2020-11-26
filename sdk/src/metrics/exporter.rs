399c32f4c350eca8a49c9f881e7ea30bf2455fc0
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-26 12:34:50 -0800

    some initial structure for exportor types

--- sdk/src/metrics/exporter.rs
+++ sdk/src/metrics/exporter.rs
@@ -1,3 +1,33 @@
+#[derive(Debug, PartialEq)]
+pub enum ExportKind {
+    Delta,
+    Cumulative,
+    PassThrough,
+}
+
+pub struct ExportKindSelector {
+    pub export: ExportKind,
+}
+
+impl ExportKindSelector {
+    pub fn select(export_kind: ExportKind) -> ExportKindSelector {
+        ExportKindSelector {
+            export: export_kind,
+        }
+    }
+}
+
+pub struct ExportRecord {
+    pub instrument: String,
+    pub label_set: String,
+    pub resource: String,
+    pub aggregation: String,
+}
+
+pub struct ExportRecordSet {
+    pub export_records: (String, String),
+}
+
 pub struct Exporter {
     pub some_collection: Vec<String>,
 }
@@ -13,6 +43,18 @@ impl Exporter {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn select() {
+        // let test_exporter = Exporter::init();
+        // assert_eq!(test_exporter.some_collection.len(), 0);
+        let test_delta = ExportKindSelector::select(ExportKind::Delta);
+        let test_cumulative = ExportKindSelector::select(ExportKind::Cumulative);
+        let test_passthrough = ExportKindSelector::select(ExportKind::PassThrough);
+        assert_eq!(test_delta.export, ExportKind::Delta);
+        assert_eq!(test_cumulative.export, ExportKind::Cumulative);
+        assert_eq!(test_passthrough.export, ExportKind::PassThrough);
+    }
 
     #[test]
     fn init() {
