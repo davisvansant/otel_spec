649b7eb1cc733ddc531ce6b8055faeb26e5d147e
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-11-27 14:18:50 -0800

    adding more structure for accumulator

--- sdk/src/metrics/accumulator.rs
+++ sdk/src/metrics/accumulator.rs
@@ -1,3 +1,5 @@
+use crate::metrics::processor::Processor;
+
 pub struct Aggregator {
     pub measurement: String,
 }
@@ -6,15 +8,34 @@ pub struct AggregatorSnapshot {
     pub aggregator: Vec<String>,
 }
 
+impl AggregatorSnapshot {
+    pub fn init() -> AggregatorSnapshot {
+        AggregatorSnapshot {
+            aggregator: Vec::with_capacity(10),
+        }
+    }
+}
+
 pub struct AggregatorSelector {
     pub aggregator: Aggregator,
 }
 
-pub struct Accumluation {
+pub struct Accumulation {
     pub instrument: String,
     pub label_set: String,
     pub resource: String,
     pub aggregator_snapshot: AggregatorSnapshot,
+}
+
+impl Accumulation {
+    pub fn init() -> Accumulation {
+        Accumulation {
+            instrument: String::with_capacity(32),
+            label_set: String::with_capacity(32),
+            resource: String::with_capacity(32),
+            aggregator_snapshot: AggregatorSnapshot::init(),
+        }
+    }
 }
 
 pub struct Aggregation {
@@ -26,16 +47,28 @@ pub enum AggregationKind {
 }
 
 pub struct Accumulator {
-    pub aggregator: Vec<String>,
-    pub accumulation: (String, String),
+    pub aggregator: Vec<Accumulation>,
+    // pub accumulation: (String, String),
+    // accumulation: Accumulation,
 }
 
 impl Accumulator {
     pub fn init() -> Accumulator {
         Accumulator {
             aggregator: Vec::with_capacity(10),
-            accumulation: (String::with_capacity(32), String::with_capacity(32)),
+            // accumulation: (String::with_capacity(32), String::with_capacity(32)),
+            // accumulation: Accumulation {
+            //     instrument: String::with_capacity(32),
+            //     label_set: String::with_capacity(32),
+            //     resource: String::with_capacity(32),
+            //     aggregator_snapshot: AggregatorSnapshot::init(),
+            // },
         }
+    }
+
+    pub fn collect(&mut self, accumulation: Accumulation, _processor: Processor) {
+        self.aggregator.push(accumulation);
+        // _processor.process()
     }
 }
 
@@ -44,12 +77,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn init() {
+    fn aggregator_snapshot_init() {
+        let test_aggregator_snapshot = AggregatorSnapshot::init();
+        assert_eq!(test_aggregator_snapshot.aggregator.len(), 0);
+    }
+
+    #[test]
+    fn accumlation_init() {
+        let test_accumulation = Accumulation::init();
+        assert_eq!(test_accumulation.instrument, String::from(""));
+        assert_eq!(test_accumulation.label_set, String::from(""));
+        assert_eq!(test_accumulation.resource, String::from(""));
+        assert_eq!(test_accumulation.aggregator_snapshot.aggregator.len(), 0);
+    }
+
+    #[test]
+    fn accumlator_init() {
         let test_accumulator = Accumulator::init();
         assert_eq!(test_accumulator.aggregator.len(), 0);
-        assert_eq!(
-            test_accumulator.accumulation,
-            (String::from(""), String::from(""))
-        );
     }
 }
