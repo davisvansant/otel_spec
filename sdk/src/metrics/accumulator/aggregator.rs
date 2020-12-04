0f5707bef89556a8396512c547586dc247ef8144
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-03 16:06:46 -0800

    initial syncronized_move tests for aggregator

--- sdk/src/metrics/accumulator/aggregator.rs
+++ sdk/src/metrics/accumulator/aggregator.rs
@@ -26,10 +26,23 @@ impl Aggregator {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use api::metrics::meter::Instrument;
 
     #[test]
     fn init() {
         let test_aggregator = Aggregator::init();
         assert_eq!(test_aggregator.instance.len(), 0);
+    }
+
+    #[test]
+    fn synchronized_move() {
+        let mut test_aggregator = Aggregator::init();
+        assert_eq!(test_aggregator.instance.len(), 0);
+        let test_meter = Meter::new(String::from("test_meter"), Instrument::Counter);
+        test_aggregator.instance.push(test_meter);
+        assert_eq!(test_aggregator.instance.len(), 1);
+        let test_snapshot = test_aggregator.synchronized_move();
+        assert_eq!(test_aggregator.instance.len(), 0);
+        assert_eq!(test_snapshot.len(), 1);
     }
 }
